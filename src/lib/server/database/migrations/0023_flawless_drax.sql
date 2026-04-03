9b489e55b83110bdc7e84efffad928cdf223074b
Richard Prins <richardprins@mac.mynetworksettings.com>
2026-04-02 22:25:31 -0600

    migration for dev db

---
+++ src/lib/server/database/migrations/0023_flawless_drax.sql
@@ -0,0 +1,13 @@
+ALTER TABLE "candidate_discipline_experience" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.136Z';--> statement-breakpoint
+ALTER TABLE "candidate_discipline_experience" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.136Z';--> statement-breakpoint
+ALTER TABLE "candidate_document_uploads" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.136Z';--> statement-breakpoint
+ALTER TABLE "candidate_document_uploads" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.136Z';--> statement-breakpoint
+ALTER TABLE "candidate_profiles" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "candidate_profiles" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "candidate_ratings" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "candidate_ratings" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "client_document_uploads" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "client_document_uploads" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.135Z';--> statement-breakpoint
+ALTER TABLE "admin_config" ALTER COLUMN "id" SET DEFAULT 'b73adfb3-2576-4039-a5b4-907d4afbb6cf';--> statement-breakpoint
+ALTER TABLE "admin_config" ALTER COLUMN "created_at" SET DEFAULT '2026-04-03T04:09:57.138Z';--> statement-breakpoint
+ALTER TABLE "admin_config" ALTER COLUMN "updated_at" SET DEFAULT '2026-04-03T04:09:57.138Z';
